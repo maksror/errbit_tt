ae98ccb36002de27fc5f7dd0c3490fa28508ae66
Maxim Rotari <maxim.rotari@orange.com>
2025-01-05 02:52:51 +0200

    Move errbit to separate user

--- recipes/default.rb
+++ recipes/default.rb
@@ -1,30 +1,3 @@
+include_recipe '::prepare_env'
 include_recipe '::mongodb'
-include_recipe '::ruby'
-include_recipe '::errbit_env'
-
-bash 'bundle exec rake' do
-    cwd '/home/errbit'
-    code 'source /etc/profile.d/rvm.sh && bundle exec rake errbit:bootstrap'
-end
-
-systemd_unit 'errbit.service' do
-    content({ 
-        Unit: {
-            Description: 'errbit',
-            After: 'network.target',
-        },
-        Service: {
-            Type: 'simple',
-            User: 'root',
-            Group: 'root',
-            WorkingDirectory: '/home/errbit',
-            ExecStart: '/bin/bash -c \'source /etc/profile.d/rvm.sh && bundle exec puma -b "tcp://0.0.0.0:3000"\'',
-            Restart: 'always',
-            KillMode: 'process',
-        },
-        Install: {
-            WantedBy: 'multi-user.target',
-        }
-    })
-  action [:create, :enable, :start]
-end
+include_recipe '::errbit_install'
