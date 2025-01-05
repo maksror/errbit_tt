6b34660e1e7bc7207c480926a14589194cc95551
Maxim Rotari <maxim.rotari@orange.com>
2025-01-05 23:28:45 +0200

    Move mongodb checks into once control block

--- test/integration/default/mongodb_service.rb
+++ test/integration/default/mongodb_service.rb
@@ -1,8 +1,12 @@
-describe port(27017) do
-    it { should be_listening }
-end
+control 'mongodb' do
+    title ''
 
-describe service('mongod') do
-    it { should be_enabled }
-    it { should be_running }
+    describe port(27017) do
+        it { should be_listening }
+    end
+
+    describe service('mongod') do
+        it { should be_enabled }
+        it { should be_running }
+    end
 end
