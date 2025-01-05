e940a4a4184ae4b1dffa1055ead5d052099f9800
Maxim Rotari <maxim.rotari@orange.com>
2025-01-05 23:30:46 +0200

    Reshuffle tests

--- test/integration/default/mongodb.rb
+++ test/integration/default/mongodb.rb
@@ -1,12 +1,12 @@
 control 'mongodb' do
     title ''
 
-    describe port(27017) do
-        it { should be_listening }
-    end
-
     describe service('mongod') do
         it { should be_enabled }
         it { should be_running }
+    end
+
+    describe port(27017) do
+        it { should be_listening }
     end
 end
