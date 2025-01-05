f1c3628b52c1498e614a0fb5438c1d832e86e5e0
Maxim Rotari <maxim.rotari@orange.com>
2025-01-05 23:36:46 +0200

    Add sleep loop to avoid situations when errbit takes a long time to start up

--- test/integration/default/errbit.rb
+++ test/integration/default/errbit.rb
@@ -1,12 +1,23 @@
 control 'errbit' do
     title ''
 
+    port_to_check = 3000
+    interval = 1
+    timeout = 10
+
+    # Avoiding situations when errbit takes a long time to start up
+    start_time = Time.now
+    loop do
+      break if port(port_to_check).listening? || (Time.now - start_time) > timeout
+      sleep(interval)
+    end
+
     describe service('errbit') do
         it { should be_enabled }
         it { should be_running }
     end
 
-    describe port(3000) do
+    describe port(port_to_check) do
         it { should be_listening }
         its('addresses') { should include '0.0.0.0' }
     end
