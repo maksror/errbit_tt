b46b5e110be4fc767cc6ebfb7c30290f3b1772c4
Maxim Rotari <maxim.rotari@orange.com>
2025-01-05 18:44:02 +0200

    Update errbit tests

--- test/integration/errbit_install/errbit_service.rb
+++ test/integration/errbit_install/errbit_service.rb
@@ -1,4 +1,13 @@
-describe service('errbit') do
-    it { should be_enabled }
-    it { should be_running }
+control 'Errbit' do
+    title 'Verify errbit application'
+
+    describe service('errbit') do
+        it { should be_enabled }
+        it { should be_running }
+    end
+
+    describe port(3001) do
+        it { should be_listening }
+        its('addresses') { should include '0.0.0.0' }
+    end
 end
